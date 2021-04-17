ab515a1e111f3bbb6aabd0d1ebb2b047db796571
DeDiamondPro <67508414+DeDiamondPro@users.noreply.github.com>
2021-04-17 18:53:58 +0200

    updated version

--- src/main/java/io/github/dediamondpro/hycord/hycord.java
+++ src/main/java/io/github/dediamondpro/hycord/hycord.java
@@ -23,7 +23,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 @Mod(modid = hycord.MODID, version = hycord.VERSION)
 public class hycord {
     public static final String MODID = "hycord";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     private final settings config = new settings();
 

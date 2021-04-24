96b10303f41b3af2920322408a2af0694395714f
DeDiamondPro <67508414+DeDiamondPro@users.noreply.github.com>
2021-04-24 19:16:29 +0200

    1.1.0

--- src/main/java/io/github/dediamondpro/hycord/hycord.java
+++ src/main/java/io/github/dediamondpro/hycord/hycord.java
@@ -30,7 +30,7 @@ import java.util.Scanner;
 @Mod(modid = hycord.MODID, version = hycord.VERSION)
 public class hycord {
     public static final String MODID = "hycord";
-    public static final String VERSION = "1.1.0-pre2.1";
+    public static final String VERSION = "1.1.0";
 
     private final Settings config = new Settings();
 
@@ -208,7 +208,7 @@ public class hycord {
                 System.out.println("File created: " + nickNameSave.getName());
                 NickNameController.nicknames.put("DeDiamondPro", "§bDeD§3iam§9ond§1Pro");
                 NickNameController.nicknames.put("Strebbypatty", "§4Strebbypatty");
-                NickNameController.nicknames.put("Unseaded", "§2Unseaded");
+                NickNameController.nicknames.put("Unseaded", "§aUn§2sea§1ded");
                 FileWriter writer = new FileWriter(String.valueOf(nickNameSave.toPath()));
                 for (String str : NickNameController.nicknames.keySet()) {
                     writer.write(str + "," + NickNameController.nicknames.get(str) + System.lineSeparator());

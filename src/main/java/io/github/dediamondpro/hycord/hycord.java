40b4d967ce19fe167b10523e54645cfe96ca197f
DeDiamondPro <67508414+DeDiamondPro@users.noreply.github.com>
2021-04-28 13:16:46 +0200

    1.1.2

--- src/main/java/io/github/dediamondpro/hycord/hycord.java
+++ src/main/java/io/github/dediamondpro/hycord/hycord.java
@@ -30,7 +30,7 @@ import java.util.Scanner;
 @Mod(modid = hycord.MODID, version = hycord.VERSION)
 public class hycord {
     public static final String MODID = "hycord";
-    public static final String VERSION = "1.1.1";
+    public static final String VERSION = "1.1.2";
 
     private final Settings config = new Settings();
 

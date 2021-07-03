08bd4a6553e033351ab3aff128a39c0fb8a216fd
Wyvest <45589059+wyvest@users.noreply.github.com>
2021-07-03 21:15:35 +0900

    Fml fml fmlf f,mflfkfmfkffflfmf

---
+++ src/main/java/io/github/dediamondpro/hycord/tweaker/FMLLoadingPlugin.java
@@ -0,0 +1,49 @@
+package io.github.dediamondpro.hycord.tweaker;
+
+import kotlin.KotlinVersion;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+
+import java.util.Map;
+
+public class FMLLoadingPlugin implements IFMLLoadingPlugin {
+
+    public FMLLoadingPlugin() {
+        if (!KotlinVersion.CURRENT.isAtLeast(1, 5, 0)) {
+            throw new RuntimeException("" +
+                    "HyCord has detected an older version of Kotlin on a mod!\n" +
+                    "The most common problem is ChatTriggers.\n" +
+                    "In order to resolve this conflict you must make HyCord be\n" +
+                    "above this mod alphabetically in your mods folder.\n" +
+                    "This tricks Forge into loading HyCord first.\n" +
+                    "You can do this by renaming your Wyvtilities jar to !Wyvtilities.jar,\n" +
+                    "or by renaming the other mod's jar to start with a Z.\n" +
+                    "If you have already done this and are still getting this error,\n" +
+                    "ask for support in the Discord (https://discord.gg/EZJwuR832S).");
+        }
+    }
+
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[0];
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+}

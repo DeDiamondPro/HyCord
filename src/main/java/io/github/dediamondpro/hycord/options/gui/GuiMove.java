1ce692394ef9e446a236bce9374ef4ceb79f1139
DeDiamondPro <67508414+DeDiamondPro@users.noreply.github.com>
2021-07-10 11:39:16 +0200

    replace "this program" with HyCord

--- src/main/java/io/github/dediamondpro/hycord/options/gui/GuiMove.java
+++ src/main/java/io/github/dediamondpro/hycord/options/gui/GuiMove.java
@@ -10,7 +10,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * along with HyCord.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package io.github.dediamondpro.hycord.options.gui;
